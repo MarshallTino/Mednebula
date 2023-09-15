09be45623f8d823958af8a68f2ad600e0ba6204a
Marcel Martino <marcelmartino2053@gmail.com>
2023-09-15 17:49:23 +0200

    Install several dependencies and create theme and globalstyles

---
+++ src/styles/Theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  fonts: {
+    primary: "Istok Web",
+    secondary: "mplus-1p",
+  },
+  colors: {
+    mainLight: "#ffffff",
+    base: "#0071BC",
+    mainDark: "#002F54",
+    mainNeutral: "#009245",
+  },
+};
+
+export default theme;
