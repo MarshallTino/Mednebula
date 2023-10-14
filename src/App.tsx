37d4d33cc6afdb0e92999442c943739323e709aa
Eric Casañé <eric.eurotete@gmail.com>
2023-10-14 18:56:27 +0200

    Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import React from "react";
 
 import "./App.css";
 import { Landing } from "./components/Landing/Landing";
+import Banner from "./components/Banner/Banner";
 
 function App() {
-  return <Landing />;
+  return (
+    <>
+      <Landing />
+      <Banner 
+        image="/images/mas-informacion.svg" 
+        buttonPosition="left" 
+        buttonText="CONTACTAR" 
+        message="¿Quieres más información?"
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,30 @@
+import BannerStyled from './BannerStyled';
+import Button from "../Button/Button";
+
+interface BannerProps {
+    image: string;
+    buttonPosition: 'left' | 'right';
+    buttonText: string;
+    message: string;
+}
+
+export const Banner = ({
+    image,
+    buttonPosition,
+    buttonText,
+    message,
+}: BannerProps): JSX.Element => {
+    return (
+        <BannerStyled className="banner" backgroundImage={image} buttonPosition={buttonPosition}>
+            <div className={`button ${buttonPosition}`}>
+                <Button
+                    className="landingpage__button"
+                    text={buttonText}
+                    disabled={false}
+                />
+            </div>
+            <h2 className='banner__title'>{message}</h2>
+        </BannerStyled>
+      );
+};
+export default Banner;
