90c35c4ca9e8066597391b503b63142b7000c144
Eric Casañé <eric.eurotete@gmail.com>
2023-10-29 18:43:39 +0100

    Added data structures

---
+++ src/data/teamData.ts
@@ -0,0 +1,27 @@
+export const teamData = [
+    {
+        name: "Laureano Nomen",
+        role: "CEO",
+        image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        name: "Angel Alcober",
+        role: "XXX",
+        image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        name: "Marcel Martino",
+        role: "XXX",
+        image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        name: "Eric Casañé",
+        role: "XXX",
+        image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+        name: "Alex Navarro",
+        role: "XXX",
+        image: "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+];
