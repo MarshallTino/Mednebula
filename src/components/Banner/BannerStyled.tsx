98c628acfa1d228cf571a65b39c885d34289f58a
Marcel Martino <marcelmartino2053@gmail.com>
2023-11-27 15:19:47 +0100

    add responsive styles to banner and footer

--- src/components/Banner/BannerStyled.tsx
+++ src/components/Banner/BannerStyled.tsx
@@ -1,45 +1,54 @@
 import styled from "styled-components";
 
 interface BannerStyledProps {
-    backgroundImage: string;
-    buttonPosition: 'left' | 'right';
+  backgroundImage: string;
+  buttonPosition: "left" | "right";
 }
 
 const BannerStyled = styled.div<BannerStyledProps>`
-width: 100%;
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-image: 
-    linear-gradient(#0071BC, #0071BC80, #0071BCC4, #0071BC),
+  background-image: linear-gradient(#0071bc, #0071bc80, #0071bcc4, #0071bc),
     url(${(props) => props.backgroundImage});
-  height: 200px;
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-  flex-direction: ${props => props.buttonPosition === 'left' ? 'row-reverse' : 'row'};
+  flex-direction: ${(props) =>
+    props.buttonPosition === "left" ? "row-reverse" : "row"};
   padding-left: 20%;
   padding-right: 20%;
-  
-  @media (max-width: 768px) {
-    flex-direction: column;
-    height: auto;
-    padding: 20px;
 
+  @media (max-width: 768px) {
+    flex-direction: column-reverse;
+    justify-content: center;
+    //height: auto;
+    padding: 20px;
+    gap: 20px;
   }
 
   .button {
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-left: ${props => props.buttonPosition === 'left' ? 'auto' : '0'};
-    margin-right: ${props => props.buttonPosition === 'right' ? 'auto' : '0'};
+    @media (min-width: 768px) {
+      margin-left: ${(props) =>
+        props.buttonPosition === "left" ? "auto" : "0"};
+      margin-right: ${(props) =>
+        props.buttonPosition === "right" ? "auto" : "0"};
+    }
   }
 
   .banner__title {
     font-family: ${(props) => props.theme.fonts.primary};
-    margin-left: ${props => props.buttonPosition === 'right' ? 'auto' : '0'};
-    margin-right: ${props => props.buttonPosition === 'left' ? 'auto' : '0'};
+    @media (min-width: 768px) {
+      margin-left: ${(props) =>
+        props.buttonPosition === "right" ? "auto" : "0"};
+      margin-right: ${(props) =>
+        props.buttonPosition === "left" ? "auto" : "0"};
+    }
     max-width: 200px;
     font-size: 30px;
     color: #fff;
