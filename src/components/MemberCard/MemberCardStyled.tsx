f3f4bc83d53c13e774ba8d9bf029b8c28d31d1e0
Marcel Martino <marcelmartino2053@gmail.com>
2023-11-27 15:12:06 +0100

    Create responsive styles on team section

--- src/components/MemberCard/MemberCardStyled.tsx
+++ src/components/MemberCard/MemberCardStyled.tsx
@@ -1,25 +1,56 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const MemberCardStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px;
+
+  @media (min-width: 480px) {
+    padding: 20px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 10px;
+  }
 
   img {
     max-width: 200px;
     height: auto;
     border-radius: 10px;
+
+    @media (max-width: 768px) {
+      max-width: 150px;
+    }
+
+    @media (max-width: 480px) {
+      max-width: 130px;
+    }
   }
 
   h3 {
     font-size: 18px;
     margin-top: 10px;
+
+    @media (max-width: 768px) {
+      font-size: 16px;
+    }
+
+    @media (max-width: 480px) {
+      font-size: 14px;
+    }
   }
 
   p {
     font-size: 14px;
     color: ${(props) => props.theme.colors.mainNeutral};
+
+    @media (max-width: 768px) {
+      font-size: 12px;
+    }
+
+    @media (max-width: 480px) {
+      font-size: 10px;
+    }
   }
 `;
 
--- src/components/TeamList/TeamListStyled.tsx
+++ src/components/TeamList/TeamListStyled.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const TeamListStyled = styled.div`
   padding-left: 80px;
@@ -6,10 +6,15 @@ const TeamListStyled = styled.div`
   margin-bottom: 80px;
   text-align: center;
   .team__list {
+    gap: 20px;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0px;
   }
 `;
 
