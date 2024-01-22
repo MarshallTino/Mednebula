a61031e8eac078ddead60c26f0a0ef841abe0ee5
Marcel Martino <marcelmartino2053@gmail.com>
2024-01-22 13:47:32 +0100

    Edit team members roles

--- src/Data/TeamMembers.ts
+++ src/Data/TeamMembers.ts
@@ -1,32 +1,38 @@
 export const TeamMembers = [
-    {
-        name: "Laureano Nomen",
-        role: "CEO",
-        image: "https://cdn.mednebula.com/static/landing/CnFlgHqxXP2QLnsQsUGbaPQubkjMXdNZN2UfW1cWVsQHV1bPaKbKRC0VrLd4ScUD/ktTF1Lx76IxpJXgj.jpg",
-    },
-    {
-        name: "Ángel Alcober",
-        role: "XXX",
-        image: "https://cdn.mednebula.com/static/landing/iZ3ebUEpyf34B3Dp05aRJZyiYuqWtexAtM3Ai7gMC7HdyYfhe9lgku7laOPjAsA1/AoJWqoImXgrE9JId.jpg",
-    },
-    {
-        name: "Marcel Martino",
-        role: "XXX",
-        image: "https://cdn.mednebula.com/static/landing/XnSywjO6SPnGhR8aCtagvVpak6DFzu1tvJSqxqtJPniG5vI5W8QhDikCgXDDBnIH/JjtxB84H2G44eWXx.jpg",
-    },
-    {
-        name: "Eric Casañé",
-        role: "XXX",
-        image: "https://cdn.mednebula.com/static/landing/VVuWZIR9vjox6IrZ0qBNcfgyQt046R9Rc5MA0h0xByNJZd5ChtKt6XBiwT8UJTmB/C1U51HFgDuiUdskF.jpg"
-    },
-    {
-        name: "Àlex Navarro",
-        role: "XXX",
-        image: "https://cdn.mednebula.com/static/landing/4YduOVWDpl6UNY0rM0Mx5IT2QvTeyGZxk991qafamrPvW1nZz8RspEoRVF37VmSn7/6wjLwJG3s1zMxUWM.jpg",
-    },
-    {
-        name: "Octavi Galli",
-        role: "XXX",
-        image: "https://cdn.mednebula.com/static/landing/f8s5b75V0abfsBqLhLjs78RuBg51fyZXbHLVZbDLR5BVQXNbSxtCl9qZFI2NYfnB/TfORAKbVxxphcYYw.jpg",
-    }
+  {
+    name: "Laureano Nomen",
+    role: "CEO",
+    image:
+      "https://cdn.mednebula.com/static/landing/CnFlgHqxXP2QLnsQsUGbaPQubkjMXdNZN2UfW1cWVsQHV1bPaKbKRC0VrLd4ScUD/ktTF1Lx76IxpJXgj.jpg",
+  },
+  {
+    name: "Ángel Alcober",
+    role: "Diseñador Gráfico",
+    image:
+      "https://cdn.mednebula.com/static/landing/iZ3ebUEpyf34B3Dp05aRJZyiYuqWtexAtM3Ai7gMC7HdyYfhe9lgku7laOPjAsA1/AoJWqoImXgrE9JId.jpg",
+  },
+  {
+    name: "Marcel Martino",
+    role: "CTO",
+    image:
+      "https://cdn.mednebula.com/static/landing/XnSywjO6SPnGhR8aCtagvVpak6DFzu1tvJSqxqtJPniG5vI5W8QhDikCgXDDBnIH/JjtxB84H2G44eWXx.jpg",
+  },
+  {
+    name: "Eric Casañé",
+    role: "Programador Fullstack",
+    image:
+      "https://cdn.mednebula.com/static/landing/VVuWZIR9vjox6IrZ0qBNcfgyQt046R9Rc5MA0h0xByNJZd5ChtKt6XBiwT8UJTmB/C1U51HFgDuiUdskF.jpg",
+  },
+  {
+    name: "Àlex Navarro",
+    role: "Desarrollador Fullstack y Ciberseguridad",
+    image:
+      "https://cdn.mednebula.com/static/landing/4YduOVWDpl6UNY0rM0Mx5IT2QvTeyGZxk991qafamrPvW1nZz8RspEoRVF37VmSn7/6wjLwJG3s1zMxUWM.jpg",
+  },
+  {
+    name: "Octavi Galli",
+    role: "Asesorador médico",
+    image:
+      "https://cdn.mednebula.com/static/landing/f8s5b75V0abfsBqLhLjs78RuBg51fyZXbHLVZbDLR5BVQXNbSxtCl9qZFI2NYfnB/TfORAKbVxxphcYYw.jpg",
+  },
 ];
